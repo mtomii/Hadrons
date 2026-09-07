11bcf4f9b18ef5c0a3c5e9f381538d1c1515484d
Masaaki Tomii <masaaki.tomii1987@gmail.com>
2026-09-07 10:08:46 +0900

    forgot to add some *.cpp files

---
+++ Hadrons/Modules/MContraction/A2ALoopNew.cpp
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MContraction/A2ALoopNew.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MContraction;
+
+template class HADRONS_NAMESPACE::MContraction::TA2ALoopNew<FIMPL>;
